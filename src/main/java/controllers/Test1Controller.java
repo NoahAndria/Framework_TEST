e483fb24abaf8cf8f2d379ea313651bb3092549f
Noah Andria <noahandriamanana41@.com>
2026-09-25 15:25:19 +0300

    tested rest api

--- src/main/java/controllers/Test1Controller.java
+++ src/main/java/controllers/Test1Controller.java
@@ -1,8 +1,12 @@
 package controllers;
 
-import annotations.Controller;
-import annotations.UrlMapping;
+import myframework.annotations.Controller;
+import myframework.annotations.RestAPI;
+import myframework.annotations.UrlMapping;
+
 import myframework.utils.ModelView;
+
+import entities.Jouet;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,7 +20,7 @@ public class Test1Controller {
 
         ModelView mv = new ModelView("random");
 
-        List<String> names = Arrays.asList("Jean", "Rakoto", "Tahiry", "Noah");
+        List<String> names = Arrays.asList("Jean", "Rakoto", "Tahiry", "Noah", "TEST");
 
         mv.addAttribute("names", names);
 
@@ -34,5 +38,19 @@ public class Test1Controller {
         mv.addAttribute("names", names);
 
         return mv;
+    }
+
+    @UrlMapping(name = "/testapi", method = "GET")
+    @RestAPI
+    public List<Jouet> hi() {
+        System.out.println("\n test of test1controller in get ");
+
+        List<Jouet> jouets = Arrays.asList(
+            new Jouet("Voiture", 10.0, "Une voiture en plastique"),
+            new Jouet("Puzzle", 15.0, "Un puzzle de 1000 pièces"),
+            new Jouet("Ballon", 5.0, "Un ballon rouge")
+        );
+
+        return jouets;
     }
 }
